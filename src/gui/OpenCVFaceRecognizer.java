1ccd3caaf0939488511de3f2b5dfb1ab1475a111
Ari Prasetiyo EBC <prasetiyooo@gmail.com>
2017-01-02 16:15:56 +0700

    upload old project

---
+++ src/gui/OpenCVFaceRecognizer.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import com.googlecode.javacv.cpp.opencv_core;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_contrib.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import org.bytedeco.javacpp.opencv_core.MatVector;
+import static org.bytedeco.javacpp.opencv_face.createFisherFaceRecognizer;
+
+public class OpenCVFaceRecognizer {
+    public static void main(String[] args) {
+        String trainingDir = args[0];
+        IplImage testImage = cvLoadImage(args[1]);
+
+        File root = new File(trainingDir);
+
+        FilenameFilter pngFilter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".png");
+            }
+        };
+
+        File[] imageFiles = root.listFiles(pngFilter);
+
+        MatVector images = new MatVector(imageFiles.length);
+
+        int[] labels = new int[imageFiles.length];
+
+        int counter = 0;
+        int label;
+
+        IplImage img;
+        IplImage grayImg;
+
+        for (File image : imageFiles) {
+            img = cvLoadImage(image.getAbsolutePath());
+
+            label = Integer.parseInt(image.getName().split("\\-")[0]);
+
+            grayImg = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
+
+            cvCvtColor(img, grayImg, CV_BGR2GRAY);
+
+            images.put(counter, (org.bytedeco.javacpp.opencv_core.Mat) grayImg);
+
+            labels[counter] = label;
+
+            counter++;
+        }
+
+        IplImage greyTestImage = IplImage.create(testImage.width(), testImage.height(), IPL_DEPTH_8U, 1);
+
+        FaceRecognizer faceRecognizer = createFisherFaceRecognizer();
+        // FaceRecognizer faceRecognizer = createEigenFaceRecognizer();
+        // FaceRecognizer faceRecognizer = createLBPHFaceRecognizer()
+
+        faceRecognizer.train(images, labels);
+
+        cvCvtColor(testImage, greyTestImage, CV_BGR2GRAY);
+
+        int predictedLabel = faceRecognizer.predict(greyTestImage);
+
+        System.out.println("Predicted label: " + predictedLabel);
+    }
+}
